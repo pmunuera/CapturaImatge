f88e8c4e3abf945404285b34edc5d167f0a518e3
pablo <pmunueragarcia.cf@iesesteveterradas.cat>
2022-12-05 16:55:13 +0100

    Commit N' Push

--- app/src/main/java/com/example/capturaimatge/MainActivity.java
+++ app/src/main/java/com/example/capturaimatge/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.capturaimatge;
 
+import static com.google.android.material.internal.ContextUtils.getActivity;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.camera.core.*;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+            }
+        });
     }
+
 }

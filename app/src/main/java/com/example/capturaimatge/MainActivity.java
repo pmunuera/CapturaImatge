19193e6ef8f415829ee8d015b97a76c18be13359
pmunuera <95918506+pmunuera@users.noreply.github.com>
2022-12-19 17:47:47 +0100

    Commit N' Push

--- app/src/main/java/com/example/capturaimatge/MainActivity.java
+++ app/src/main/java/com/example/capturaimatge/MainActivity.java
@@ -4,12 +4,16 @@ import static com.google.android.material.internal.ContextUtils.getActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.camera.core.*;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.*;
 import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,9 +27,29 @@ public class MainActivity extends AppCompatActivity {
 
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-
+                if (hasCameraPermission()) {
+                    enableCamera();
+                } else {
+                    requestPermission();
+                }
             }
         });
     }
-
+    private boolean hasCameraPermission() {
+        return ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED;
+    }
+    private void requestPermission() {
+        /*ActivityCompat.requestPermissions(
+                this,
+                CAMERA_PERMISSION,
+                CAMERA_REQUEST_CODE
+        );*/
+    }
+    private void enableCamera() {
+        Intent intent = new Intent(this, CameraActivity.class);
+        startActivity(intent);
+    }
 }
